ed5b85f7a659cdd7e104baab285178a708bc7536
eminbasbayan <emin.basbayan@icloud.com>
2023-11-01 16:58:49 +0300

    python'da mantıksal operatörler

---
+++ 8- mantiksal-operatorler.py
@@ -0,0 +1,7 @@
+fiyat = 5
+# and: İki ifade de True ise True döndürür.
+print(fiyat > 10 and fiyat < 30)
+# or: En az bir ifade True ise True döndürür.
+print(fiyat > 10 or fiyat < 30)
+# not: Boolean değeri tersine çevirir.
+print(not (fiyat < 10))
