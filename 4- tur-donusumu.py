c4b71e4c6ac8bd99f28ba584d06f5fbf8768141a
eminbasbayan <emin.basbayan@icloud.com>
2023-11-01 11:42:56 +0300

    python'da tip dönüşümü

---
+++ 4- tur-donusumu.py
@@ -0,0 +1,17 @@
+""" 
+number: 10, 20, 30, 1000,
+string: "Emin Başbayan", "BilGen Yazılım Akademi", "10",
+boolean: True, False
+"""
+# Birinci örnek - Tam sayı:
+""" 
+dogum_yili = int(input("Doğum yılınızı giriniz: "))
+yas = 2024 - dogum_yili
+print(yas)
+"""
+
+# İkinci örnek - Ondalıklı sayı:
+birinci_sayi = float(input("Birinci sayıyı giriniz: "))
+ikinci_sayi = float(input("İkinci sayıyı giriniz: "))
+toplam = birinci_sayi + ikinci_sayi
+print("Toplam: ", toplam)
