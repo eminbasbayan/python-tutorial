b552e701dae0d8ac7e621cb7a92f99fd2b44ef47
eminbasbayan <emin.basbayan@icloud.com>
2023-11-01 14:31:22 +0300

    python'da aritmetik operatörler

---
+++ 6- aritmetik-operatorler.py
@@ -0,0 +1,12 @@
+print(20 + 7)
+print(20 - 7)
+print(20 * 7)
+print(20 / 7) # ondalıklı bölme yapar
+print(20 // 7) 
+print(20 % 7) # mod alma
+print(10 ** 2)
+x = 20
+# x = x + 3
+x += 3
+print(20 + 5 * 2)
+print(x)
