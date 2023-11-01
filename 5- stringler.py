1c2b6b7d7204cc00a03a48790d5d0faf8ffa9d52
eminbasbayan <emin.basbayan@icloud.com>
2023-11-01 13:46:47 +0300

    python'da metin işleme: strings

---
+++ 5- stringler.py
@@ -0,0 +1,8 @@
+kurs_ismi = "Python Hızlı Başlangıç Kursu"
+print(kurs_ismi.upper())
+print(kurs_ismi.lower())
+print(kurs_ismi.find("P"))
+print(kurs_ismi.find("Python"))
+print(kurs_ismi.replace("Python","JavaScript"))
+print("Python" in kurs_ismi)
+print("JavaScript" in kurs_ismi)
